dd259b394be1de8d512b002ce63abf89476e5ef1
safwanvk <safwanvalakundil@gmail.com>
2020-09-19 19:30:50 +0530

    :zap: fetch data display in table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './home/home.component';
-import { FriendComponent } from './friend/friend.component';
+import { HttpClientModule } from '@angular/common/http'
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    FriendComponent
+    HomeComponent
+  
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http'
+
+export class Friend {
+  constructor(
+    public id: number,
+    public name: string,
+    public username: string,
+    public phone: string,
+    public email: string,
+    public website: string
+  ){
+
+  }
+}
 
 @Component({
   selector: 'app-home',
@@ -6,10 +20,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  friends: Friend[];
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.getFriends();
+  }
+  getFriends(){
+    this.http.get<any>('https://jsonplaceholder.typicode.com/users').subscribe(
+      response => {
+        console.log(response);
+        this.friends = response;
+      }
+    );
   }
 
 }
