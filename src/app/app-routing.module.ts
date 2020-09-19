4a06ea2839e393b50e08df279f0595f24b3dbd4e
safwanvk <safwanvalakundil@gmail.com>
2020-09-19 16:26:39 +0530

    :zap: Add components home, friend and set route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { FriendComponent } from './friend/friend.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'friends', component: FriendComponent},
+  {path: '', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
